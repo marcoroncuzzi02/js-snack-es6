b880b6f28f5bac0d8c1ffb25a051c5b0d550ea4c
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-23 12:15:54 +0100

    START

---
+++ main.js
@@ -0,0 +1,36 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+let pesoBiciclette =[]
+const biciclette = [
+    {
+        nome : 'biciDaCorsa',
+        peso : 15
+    },
+    {
+        nome : 'biciDaCitta'
+        peso : 25
+    },
+    {
+        nome : 'biciDaMountainBike'
+        peso : 20
+    },
+    {
+        nome : 'biciDaBambino',
+        peso : 12
+    }
+]
+
+
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+// Snack 3 (Bonus)
+// Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).
